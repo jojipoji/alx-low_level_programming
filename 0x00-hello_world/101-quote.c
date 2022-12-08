f6200bd99b07c4849af07abf0ff80aa59408aec5
jojipoji <jojeepoji@gmail.com>
2022-12-08 13:26:54 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 /**
  *main - Entry point
  *Return: Always 1 (success)
  */
 int main(void)
 {
-write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
