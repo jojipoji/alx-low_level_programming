458f340962836eb29cf2c4f13cd0aa2f7eed53c8
jojipoji <jojeepoji@gmail.com>
2022-12-08 08:46:05 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-#include <stdio> int main() { puts("Programming is like building a multilingual puzzle\n"); return (0) }
+int main() { puts("Programming is like building a multilingual puzzle\n"); return (0) }
