eaccb915abc76d7671cbf782fdb398fd61c559d2
jojipoji <jojeepoji@gmail.com>
2022-12-08 09:49:24 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
 int main(void)
-  
+
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  Return (0);
+  puts("\"Programming is like building a multilingual puzzle\n");
+
+  return (0);
+}
