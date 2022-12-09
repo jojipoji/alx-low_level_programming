abd10c5642c4d67d0b38de104de093276a4f67f8
jojipoji <jojeepoji@gmail.com>
2022-12-09 14:31:17 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
 putchar('\n');
 return(0)
 }
