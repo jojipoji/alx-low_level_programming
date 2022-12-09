670db29c4599f7c3cecdb8e87bb9abca64a35012
jojipoji <jojeepoji@gmail.com>
2022-12-09 10:01:05 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,14 +11,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("n%10 and is greater than 5\n", n);
+printf("%d and is greater than 5\n", n);
 }
 else if (n == 0)
 {
-printf("n%10 and is 0\n", n)
+printf("%d and is 0\n", n)
 }
 else (n < 6 AND != 0)
-{printf("n%10 and is less than 6 and not 0\n", n);
+{printf("%d and is less than 6 and not 0\n", n);
 }
  return (0);
 }
