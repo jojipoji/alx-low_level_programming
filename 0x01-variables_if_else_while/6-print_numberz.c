a9a911dcbc914893b133595e7349c084989d0e2d
jojipoji <jojeepoji@gmail.com>
2022-12-09 15:45:41 -0800

    putchar numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - numbers of base 10
+*Return: Always 0
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
