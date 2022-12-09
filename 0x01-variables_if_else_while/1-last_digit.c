898f1bfb669365d5ac69c846dd28c99c24b82b53
jojipoji <jojeepoji@gmail.com>
2022-12-09 12:10:05 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,16 @@ int last_num;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_num = n%10;
-printf("last digit of %d is %d ", n,last_num);
-if (last_num > 5)
+printf("last digit of %d is %d ",n,last_num);
+if(last_num > 5)
 {
 printf("and is greater than 5\n", n);
 }
-else if (last_num == 0)
+else if(last_num == 0)
 {
 printf("and is 0\n", n);
 }
-else (last_num < 6 && last_num != 0)
+else if(last_num < 6 && last_num != 0)
 {printf("and is less than 6 and not 0\n", n);
 }
 return (0);
