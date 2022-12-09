b33a9e39406c294b1e159ab0dd93240b26dcc449
jojipoji <jojeepoji@gmail.com>
2022-12-09 14:24:57 -0800

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <ctype.h>
 #include <stdlib.h>
 /**
- *main - prints characters a-z in small letters
- *Return: Always 0 (Success)
- */
+*main - prints characters a-z in small letters
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-putchar("\n");
-return (0);
+putchar('\n');
+return(0)
 }
