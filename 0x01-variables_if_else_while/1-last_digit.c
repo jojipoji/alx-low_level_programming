2094c446235073c0cd677ac3b6e4dbfee34ebb19
jojipoji <jojeepoji@gmail.com>
2022-12-09 10:06:07 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ printf("%d and is greater than 5\n", n);
 }
 else if (n == 0)
 {
-printf("%d and is 0\n", n)
+printf("%d and is 0\n", n);
 }
-else (n < 6 AND != 0)
+else (n < 6) AND (!= 0)
 {printf("%d and is less than 6 and not 0\n", n);
 }
- return (0);
+return (0);
 }
