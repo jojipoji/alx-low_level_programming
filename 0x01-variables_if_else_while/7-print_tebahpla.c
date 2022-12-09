6814eb9415cafb41a6c208fbfeea34d29ca68054
jojipoji <jojeepoji@gmail.com>
2022-12-09 15:58:00 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Reverse alphabet
+ *Return: Always 0
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter >= 'z'; letter--)
+putchar(letter);
+putchar('\n');
+return(0);
+}
