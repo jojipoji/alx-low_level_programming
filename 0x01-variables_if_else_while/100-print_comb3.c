12322d2396f109147732bd132feaf1b10529e896
jojipoji <jojeepoji@gmail.com>
2022-12-11 12:18:22 -0800

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,17 @@ int main(void)
 {
 int i, j;
 for (i = 0; i < 9; i++)
-    
-{for (j= i + 1; j < 10; j++)
+for (j= i + 1; j < 10; j++)
+{
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
-if (i == 8 && j == 9)
+if (i = = 8 && j = = 9)
 continue;
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-return(0);
+return (0);
 }
