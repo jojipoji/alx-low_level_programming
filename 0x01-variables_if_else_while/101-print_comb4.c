22cbe99af4f5da6c430c6f05741aaf70cbe46879
jojipoji <jojeepoji@gmail.com>
2022-12-11 13:15:08 -0800

    101 print combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  *main entry point
+ *101combo digits
  *Return: Always 0
  */
 int main(void)
 {
 int i, j, h;
-for (i=0;i<8;i++)
-for (j=i+1;j<9;j++)
-for (h=j+1;h<10;h++)
-{putchar((i % 10) + '0');
-putchar((j %10) + '0');
-putchar((h %10) + '0');
+for (i = 0; i < 8; i++)
+for (j = i+1; j < 9; j++)
+for (h = j+1; h < 10; h++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar((h % 10) + '0');
 if (i == 7 && j == 8 && h == 9)
 continue;
 putchar(',');
