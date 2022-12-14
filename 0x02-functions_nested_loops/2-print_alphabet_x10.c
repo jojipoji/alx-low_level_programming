d810be421808255c336f7ad973084a115c0e4a23
jojipoji <jojeepoji@gmail.com>
2022-12-14 04:45:29 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Print alphabet ten times
+ *Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+char j;
+char i;
+for (i = 0; i <= 9; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+}
+}
