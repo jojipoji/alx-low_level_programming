bfaac1157efb24bf0a6e7633bac7360772438565
jojipoji <jojeepoji@gmail.com>
2022-12-14 04:17:49 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *main - print alpahbets
+ *print_alphabet - print alpahbets
  *Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-  char j;
-  for (j = 'a'; j <= 'z'; j++)
-    {
-     _putchar(j);
-    }
-  _putchar('\n');
+char j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
 }
