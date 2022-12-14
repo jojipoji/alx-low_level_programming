225352c5f1e85763d40df4433927aaff13f626a6
jojipoji <jojeepoji@gmail.com>
2022-12-14 04:04:40 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - print alpahbets
+ *Return: Always 0
+ */
+void print_alphabet(void);
+{
+  char j;
+  for (j = a; j <= z; j++)
+    {
+      putchar(j);
+    }
+  putchar('\n');
+  return (0);
+}
