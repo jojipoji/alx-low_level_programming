4477c27aa3fc8ef2b7fd173e494678b4eac8adca
jojipoji <jojeepoji@gmail.com>
2022-12-15 06:02:51 -0800

    lets debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
 /**
- *positive_or_negative - Tests the function if the integer
+ *main - Tests the function if the integer
  *Return: Always 0
+ *
+ *
  */
-int positive_or_negative(void)
+int main(void)
 {
 int i;
 i = 0;
