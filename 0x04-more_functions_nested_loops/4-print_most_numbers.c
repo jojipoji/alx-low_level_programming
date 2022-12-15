3f456fe17772691974747120798ef43071e3da3f
jojipoji <jojeepoji@gmail.com>
2022-12-15 11:03:21 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_most_numbers - print number 0-9 without 2 or 4
+ *Return: Nothing
+ */
+void print_most_numbers(void);
+{
+int = 0;
+while (n = < 10)
+{
+if (n != 2 && n != 4)
+putchar('n');
+n++;
+}
+putchar('\n');
+}
