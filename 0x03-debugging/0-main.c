8ef472cb27f0754b1bca0fc40179fc0cdae65cd4
jojipoji <jojeepoji@gmail.com>
2022-12-15 03:21:25 -0800

    lets debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *main - Tests the function positive_or_negative()
+ *main - Tests the function if the integer is positive or negative
  *Return: Always 0
  */
 int main(void)
 {
 int i;
 i = 98;
-positive_or_negative(i);
+positive_or_negative(i):
 return (0);
 }
