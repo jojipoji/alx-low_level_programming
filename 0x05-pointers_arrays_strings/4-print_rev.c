a759db86de8c0bed1d217d75800cc99d4ab76a99
jojipoji <jojeepoji@gmail.com>
2022-12-19 09:46:21 -0800

    length in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,10 @@
  */
 void print_rev(char *s)
 {
-int i;
-for (i = _strlen(s) - 1; i >= 0; i--)
+int i, len = strlen(s);
+for (i = len - 1; i >= 0; i--)
 {
-_putchar(*(s + i));
+putchar(s[i]);
 }
-_putchar('\n');
+putchar('\n');
 }
