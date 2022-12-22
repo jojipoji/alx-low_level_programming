bcb9b62c445e4f00ba9901371cd32e6eca8dac3f
jojipoji <jojeepoji@gmail.com>
2022-12-22 09:28:43 +0300

    concatinating using n bytes from the source

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = strlen(dest);
-for (int i = 0; i < n && src[i] != '\0'; i++) {
-dest[dest_len + i] = src[i];
-}
-dest[dest_len + n] = '\0';
-return dest;
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
