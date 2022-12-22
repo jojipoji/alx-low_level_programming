45e20f828c06af5ea276f9cae53c9ca8b3e266a4
jojipoji <jojeepoji@gmail.com>
2022-12-22 11:52:25 +0300

    a function to copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,4 @@
 #include "main.h"
-/**
- #include "holberton.h"
  /**
  * _strncpy - copies a string
  * @dest: destination to copy
@@ -10,15 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
-
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  for (; i < n; i++)
-    {
-      dest[i] = '\0';
-    }
-  return (dest);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
