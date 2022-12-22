6fc13dc60df6ebe19475bf61cd7d9cf27f92b960
jojipoji <jojeepoji@gmail.com>
2022-12-22 12:33:25 +0300

    betty corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,25 @@
  *
  * Return: value of string
  */
-int _strcmp(char *s1, char *s2) {
-while (*s1 != '\0' && *s2 != '\0') {
-if (*s1 != *s2) {
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2)
+{
 return *s1 - *s2;
 }
 s1++;
 s2++;
 }
-if (*s1 == '\0' && *s2 == '\0') {
+if (*s1 == '\0' && *s2 == '\0')
+{
 return 0;
-} else if (*s1 == '\0') {
+} else if (*s1 == '\0')
+{
 return -1;
-} else {
+} else
+{
 return 1;
 }
 }
