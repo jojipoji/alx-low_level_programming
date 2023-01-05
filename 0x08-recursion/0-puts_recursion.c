8624d49800f0b90e2972e7bc1c98359f21073152
jojipoji <jojeepoji@gmail.com>
2023-01-05 19:47:40 +0300

    first recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_puts_recursion - prints a string, followed by a new line.
+ *@s: string to be printed
+ *Return : always (0)
+ */
+void _puts_recursion(char *s)
+{
+  if (*s)
+    {
+      _putchar (*s);
+      _puts_recursion(++s);
+    }
+  else
+    _putchar('\n');
+}
