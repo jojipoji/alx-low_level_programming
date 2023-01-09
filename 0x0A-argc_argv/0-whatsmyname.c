04ab7553ace3d4d28fd4672eeb6925f752f617b6
jojipoji <jojeepoji@gmail.com>
2023-01-09 15:30:13 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *Main - main function
+ *@argc: arguments count
+ *@argv: arguments vector
+ *Return: Always o
+ */
+int main(int argc, char*argv[])
+{
+  unused(argc);
+  printf("%s\n", argv[0]);
+  return (0);
+}
