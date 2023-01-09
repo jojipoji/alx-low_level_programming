b8c04b52f6dc20317613d29e155df12db0a97a15
jojipoji <jojeepoji@gmail.com>
2023-01-09 15:38:17 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#define unused (x) (void)(0)
+#define unused(x) (void)(x)
 /**
  *Main - main function
  *@argc: arguments count
  *@argv: arguments vector
  *Return: Always o
  */
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-  unused(argc);
-  printf("%s\n", argv[0]);
-  return (0);
+unused(argc);
+printf("%s\n", argv[0]);
+return (0);
 }
