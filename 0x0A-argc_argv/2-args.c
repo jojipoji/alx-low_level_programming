2f9d60f922ace277b4383241de4056500e52b594
jojipoji <jojeepoji@gmail.com>
2023-01-09 16:51:54 +0300

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#define unused(x) void(x)
 /**
  *main - main function
  *@argc: arguments count
  *@argv: arguments vector
  *Return: Always 0
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
