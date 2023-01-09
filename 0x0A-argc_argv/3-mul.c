5ddb4a0887985e77fe649ad0f7982ca1a45d5469
jojipoji <jojeepoji@gmail.com>
2023-01-09 18:28:26 +0300

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main function
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+if argc == 3
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+printf("error\n");
+}
