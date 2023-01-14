2d751a10f57da46e1be61303ee24f5629d28180f
jojipoji <jojeepoji@gmail.com>
2023-01-14 16:31:49 +0300

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,16 +8,15 @@
  */
 int *array_range(int min, int max)
 {
-int i, size, *array;
+int i, size;
+int *array;
 if (min > max)
 return (NULL);
 size = max - min + 1;
-array = malloc(size *sizeof(int));
+array = malloc(sizeof(int) * size);
 if (array == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
-{
-array[i] = min + 1;
-}
+array[i] = min++;
 return (array);
 }
