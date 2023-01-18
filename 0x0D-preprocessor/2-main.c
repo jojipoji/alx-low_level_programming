5576b30521af5afdca2c05171aa3cf32c0fb74a5
jojipoji <jojeepoji@gmail.com>
2023-01-18 08:06:01 +0300

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-void int main
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints the nameof the file its written on
+@__FILE__: macro name
+Return: always 0
+*/
+int main()
 {
 printf("%s\n", __FILE__);
 return (0);
