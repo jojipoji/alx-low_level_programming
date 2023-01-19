86912ac839bd8cbe8fffd45de03f34b8acbef624
jojipoji <jojeepoji@gmail.com>
2023-01-19 20:41:48 +0300

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - indexes array
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to use with size
+ * Return: int value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || cmp == NULL || array == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
+}
