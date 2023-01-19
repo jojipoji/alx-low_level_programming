84aebc2277681d98a1349b89d1180da961ad0515
jojipoji <jojeepoji@gmail.com>
2023-01-19 20:29:38 +0300

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
-if (array == NULL && action == NULL)
+if (array == NULL || action == NULL)
 exit(98);
 for (i = 0; i < size; i++)
 {
