ff890c85d215c421cc413b0c627e779ddf5b0e23
jojipoji <jojeepoji@gmail.com>
2023-02-10 16:51:38 +0300

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
+#ifndef IO_
+#define IO_
+int _putchar(char c);
+int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif /* HOLBERTON_H */
+#endif
